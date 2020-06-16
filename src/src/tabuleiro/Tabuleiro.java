f29e0093a51a07b0d148b097385e52a663aec1c8
Jeferson <jeferson.santos@ilog.com.br>
2020-06-16 20:59:48 -0300

    Classe Tabuleiro e Peças

--- src/src/tabuleiro/Tabuleiro.java
+++ src/src/tabuleiro/Tabuleiro.java
@@ -6,12 +6,12 @@ package tabuleiro;
 public class Tabuleiro {
     private int linhas;
     private int colunas;
-    private Peca[][] peca;
+    private Peca[][] pecas;
 
     public Tabuleiro(int linhas, int colunas) {
         this.linhas = linhas;
         this.colunas = colunas;
-        peca = new Peca[linhas][colunas];
+        pecas = new Peca[linhas][colunas];
     }
 
     public int getLinhas() {
@@ -28,5 +28,11 @@ public class Tabuleiro {
 
     public void setColunas(int colunas) {
         this.colunas = colunas;
+    }
+    public Peca peca(int linha, int coluna){
+        return pecas[linha][coluna];
+    }
+    public Peca peca(Posicao posicao){
+        return pecas[posicao.getLinha()][posicao.getColuna()];
     }
 }
